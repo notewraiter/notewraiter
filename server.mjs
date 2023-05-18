b65d63d1a290db16f77df03f2a96071211b9e5f2
Boston Gubler <63676517+AbsoluteTruthist@users.noreply.github.com>
2023-05-17 21:02:37 -0600

    Draft #4

---
+++ server.mjs
@@ -0,0 +1,97 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import { FormData } from 'formdata-node';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import multer from 'multer';
+import pkg from 'blob';
+const { Blob } = pkg;
+import { File } from 'formdata-node';
+import fs from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const API_KEY = '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';
+const app = express();
+const port = 3000;
+
+const upload = multer({ storage: multer.memoryStorage() });
+app.use(express.json());
+
+
+app.post('/whisper/asr', upload.single('audio'), async (req, res) => {
+  const audioBuffer = Buffer.from(req.file.buffer);
+  const audioFile = new File([audioBuffer], 'audio.webm', { type: 'audio/webm' });
+  const form = new FormData();
+  form.append('file', audioFile);
+  form.append('model', 'whisper-1');
+
+  // Save the converted audio file to disk
+  const outputFilePath = __dirname + '/output.webm';
+  await fs.writeFile(outputFilePath, audioBuffer);
+
+  try {
+    const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+      },
+      body: form,
+    });
+
+    const data = await response.json();
+    console.log('Whisper API response:', JSON.stringify(data));
+    res.json({ transcription: data.text });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error while transcribing.' });
+  }
+});
+
+app.post('/chatgpt', async (req, res) => {
+  try {
+    const userMessage = req.body.message;
+
+    const requestBody = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: 'You are an amazing medical scribe. You take transcriptions of conversations between doctors and patients, you pull out the relevant medical information, and you put it all into a SOAP note. Anything you do not know, from the conversation, you put *** instead. You never leave anything out from the conversation, but you only include things that you read in the transcript. You are really good at not putting information into the note more than once. You write the note in the first person. The 4 headings of a SOAP note are Subjective, Objective, Assessment and Plan. Each heading is described below. Subjective: This is the first heading of the SOAP note. Documentation under this heading comes from the “subjective” experiences, personal views or feelings of a patient or someone close to them. In the inpatient setting, interim information is included here. This section provides context for the Assessment and Plan. Chief Complaint (CC) The CC or presenting problem is reported by the patient. This can be a symptom, condition, previous diagnosis or another short statement that describes why the patient is presenting today. The CC is similar to the title of a paper, allowing the reader to get a sense of what the rest of the document will entail. Examples: chest pain, decreased appetite, shortness of breath. However, a patient may have multiple CC’s, and their first complaint may not be the most significant one. Thus, physicians should encourage patients to state all of their problems, while paying attention to detail to discover the most compelling problem. Identifying the main problem must occur to perform effective and efficient diagnosis. History of Present Illness (HPI)The HPI begins with a simple one line opening statement including the patients age, sex and reason for the visit.Example: 47-year old female presenting with abdominal pain. This is the section where the patient can elaborate on their chief complaint. An acronym often used to organize the HPI is termed “OLDCARTS”:Onset: When did the CC begin? Location: Where is the CC located? Duration: How long has the CC been going on for? Characterization: How does the patient describe the CC? Alleviating and Aggravating factors: What makes the CC better? Worse? Radiation: Does the CC move or stay in one location? Temporal factor: Is the CC worse (or better) at a certain time of the day? Severity: Using a scale of 1 to 10, 1 being the least, 10 being the worst, how does the patient rate the CC? It is important for clinicians to focus on the quality and clarity of their patients notes, rather than include excessive detail. History Medical history: Pertinent current or past medical conditions. Surgical history: Try to include the year of the surgery and surgeon if possible.Family history: Include pertinent family history. Avoid documenting the medical history of every person in the patients family. Social History: An acronym that may be used here is HEADSS which stands for Home and Environment; Education, Employment, Eating; Activities; Drugs; Sexuality; and Suicide/Depression. Review of Systems (ROS) This is a system based list of questions that help uncover symptoms not otherwise mentioned by the patient.General: Weight loss, decreased appetite. Gastrointestinal: Abdominal pain, hematochezia. Musculoskeletal: Toe pain, decreased right shoulder range of motion. Current Medications, Allergies. Current medications and allergies may be listed under the Subjective or Objective sections. However, it is important that with any medication documented, to include the medication name, dose, route, and how often. Example: Motrin 600 mg orally every 4 to 6 hours for 5 days Objective This section documents the objective data from the patient encounter. This includes: Vital signs Physical exam findings Laboratory data Imaging results Other diagnostic data Recognition and review of the documentation of other clinicians. A common mistake is distinguishing between symptoms and signs. Symptoms are the patients subjective description and should be documented under the subjective heading, while a sign is an objective finding related to the associated symptom reported by the patient. An example of this is a patient stating he has “stomach pain,” which is a symptom, documented under the subjective heading. Versus “abdominal tenderness to palpation,” an objective sign documented under the objective heading. Assessment: This section documents the synthesis of “subjective” and “objective” evidence to arrive at a diagnosis. This is the assessment of the patient’s status through analysis of the problem, possible interaction of the problems, and changes in the status of the problems. Elements include the following. Problem List the problem list in order of importance. A problem is often known as a diagnosis. Differential Diagnosis This is a list of the different possible diagnosis, from most to least likely, and the thought process behind this list. This is where the decision-making process is explained in depth. Included should be the possibility of other diagnoses that may harm the patient, but are less likely. Example: Problem 1, Differential Diagnoses, Discussion, Plan for problem 1 (described in the plan below). Repeat for additional problems. Plan This section details the need for additional testing and consultation with other clinicians to address the patients illnesses. It also addresses any additional steps being taken to treat the patient. This section helps future physicians understand what needs to be done next. For each problem: State which testing is needed and the rationale for choosing each test to resolve diagnostic ambiguities; ideally what the next step would be if positive or negative. Therapy needed (medications). Specialist referral(s) or consults. Patient education, counseling A comprehensive SOAP note has to take into account all subjective and objective information, and accurately assess it to create the patient-specific assessment and plan.',
+          },
+        {
+          role: 'user',
+          content: userMessage,
+        },
+      ],
+    };
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    const data = await response.json();
+    const message = data.choices && data.choices.length > 0 ? data.choices[0].message.content.trim() : '';
+    res.json({ reply: message });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname + '/public/' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
